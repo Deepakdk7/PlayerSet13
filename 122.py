2f6c359b8d43b1ccba3420c59b1262ae18476636
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-18 11:55:03 +0530

    Create 122.py

---
+++ 122.py
@@ -0,0 +1,4 @@
+ax=list(map(int,input().split('-')))
+d={1:'January',2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}
+c=d.get(ax[1])
+print(c)
