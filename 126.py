7057d72a03b7849dede1325a90a61740630e951b
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-18 13:10:58 +0530

    Create 126.py

---
+++ 126.py
@@ -0,0 +1,13 @@
+ax=list(map(int,input().split()))
+a=input().split()
+z=[]
+k=ax[1]
+n=ax[0]
+for i in range(0,n):
+    c=0
+    for j in range(0,n):
+        if a[i]==a[j]:
+            c=c+1
+    if c<k:
+        z.append(a[i])
+print(*sorted(z))
